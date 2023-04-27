fb4bf5d8cb8201d5e4c3355ab41ce13d8a9a01b8
henryamos <amoshenry95@gmail.com>
2023-04-27 02:14:36 +0000

    Rest Country API is added,the select region and search country input is displaying data based on the input value

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect,useRef } from 'react';
 import React from 'react'
 import Header from './components/Header';
 import Country from './components/Country';
@@ -8,11 +8,88 @@ import  {Routes,Route} from "react-router-dom"
 import CountryDetails from './components/CountryDetails';
 
 function App() {
-//  dark mode state : set to false //
-const [darkMode,setDarkMode] = useState(false);
-const switchMode = ()=>{
-  setDarkMode((prevState) => !prevState);
-}  
+  const url = 'https://restcountries.com/v3.1/all';
+  //  dark mode state : set to false //
+  const [darkMode, setDarkMode] = useState(false);
+  //Search State
+  const countriesInputRef = useRef();
+  const regionRef = useRef();
+  //Countries state //
+  const [countries, setCountries] = useState([]);
+
+  const noCountries = countries.status || countries.message;
+
+  const fetchCountryData = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      if (data.status === 404) {
+        setCountries([]);
+        return;
+      }
+      setCountries(data);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    //Fetch data Function
+    fetchCountryData();
+  }, []);
+
+  //dark mode function //
+  const switchMode = () => {
+    setDarkMode((prevState) => !prevState);
+  };
+  // Searching Countries //
+  const searchCountries = () => {
+    const searchValue = countriesInputRef.current.value;
+    if (searchValue.trim()) {
+      const fetchSearch = async () => {
+        const response = await fetch(
+          `https://restcountries.com/v3.1/name/${searchValue}`
+        );
+        const data = await response.json();
+        setCountries(data);
+      };
+      try {
+        fetchSearch();
+      } catch (error) {}
+    } else {
+      fetchCountryData();
+    }
+  };
+  // select region //
+  const selectRegion = () => {
+    const selectValue = regionRef.current.value;
+    if (selectValue.trim()) {
+      const fetchSelect = async () => {
+        const response = await fetch(
+          `https://restcountries.com/v3.1/region/${selectValue}`
+        );
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        if (selectValue === 'All') {
+          try {
+            fetchCountryData();
+          } catch (error) {
+            console.log(error);
+          }
+          return;
+        }
+        setCountries(data);
+      };
+      try {
+        fetchSelect();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
 return (
     <div className={`app ${darkMode ? 'darkMode' :''}`}>
       {/* Header Section */}
@@ -27,24 +104,37 @@ return (
             {/* Search Input */}
             <div className= {`search_input ${darkMode ? 'darkMode' :''}`}>
               <SearchIcon/>
-              <input type="search"  placeholder='Search for country...' />
+              <input type="search"  placeholder='Search for country...' ref={countriesInputRef} onChange={searchCountries}/>
             </div>
   
             {/* Region Selection */}
             <div className={`region ${darkMode ? 'darkMode' :''}`}>
-                <select>
-                  <option value="">All</option>
-                  <option value="">Africa</option>
-                  <option value="">America</option>
-                  <option value="">Asia</option>
-                  <option value="">Europe</option>
-                  <option value="">Ocenia</option>
+                <select ref={regionRef} onChange={selectRegion}>
+                  <option>All</option>
+                  <option>Africa</option>
+                  <option>Americas</option>
+                  <option>Asia</option>
+                  <option>Europe</option>
+                  <option>Ocenia</option>
                 </select>
             </div>
           </div>
           {/* Countries Container */}
           <div className="countries">
-            <Country darkMode={darkMode}/>
+              {!noCountries? (
+
+                countries.map(country =>(
+                  <Country 
+                    darkMode={darkMode}
+                    key={country.name.common} {...country}
+                  />
+                ))
+
+                  ):(
+                    <h4>No Country Found..</h4>
+                  )
+         
+            }
           </div>
         </div>
       
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
-
-const Country = ({darkMode}) => {
+const Country = ({darkMode,flags,name,region,capital,population}) => {
   return (
+   
     <div className={`country ${darkMode ? 'darkMode' :''}`}>
+    
       <div className="flag_container">
-        <img src="" alt="" />
+        <img src={flags.svg} alt="" />
       </div>
 
       <div className="details">
-        <h3 className="name">Name</h3>
+        <h3 className="name">{name.common}</h3>
         <p className='title'> Population:
-        <span className={`values ${darkMode ? 'darkMode' :''}`}>Test</span>
+        <span className={`values ${darkMode ? 'darkMode' :''}`}>{population}</span>
         </p>
         <p className='title'> Capital:
-        <span className={`values ${darkMode ? 'darkMode' :''}`}>Test</span>
+        <span className={`values ${darkMode ? 'darkMode' :''}`}>{capital}</span>
         </p>
-        <p className='title'> Region:
-        <span className={`values ${darkMode ? 'darkMode' :''}`}>Test</span>
+        <p className='title'> Region:{''}
+        <span className={`values ${darkMode ? 'darkMode' :''}`}>{region}</span>
         </p>
         
       </div>
